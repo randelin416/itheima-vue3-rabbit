9e67d3b3bf7315db8a0146985a0ca789c458b48c
randelin416 <konankid10129@gmail.com>
2024-06-18 11:47:02 +0800

    take note, import and setup elementPlus, test elementPlus with button

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// elementPlus按需導入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // elementPlus
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    // 實質路徑轉換 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
